d2755021238ce43c9b200dea1024ef2f12914187
Jon Haxhaj <127367051+jhaxhaj@users.noreply.github.com>
2024-01-18 10:47:32 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -3,15 +3,23 @@ def hello_world():
     # description of what the function does.
     """
 
+    This is a doc string. These are quite helpful as they can be multi-lined where as comments must have a # symbol before each line
+    They can also be denoted using a single apostrphe three times such as ''' ''' 
+
     """
 
     # Write a comment; comments are very useful to other programmers.
 
+    # THIS IS A COMMENT. As denoted by the #symbol
+
     # Write a Python statement to print Hello, World!
+
+    print("Hello, World!")
 
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    
+    output = a + b
 
     return output
